1d3b8888f52c4ff92b9e82febd09e3ccb3a47d6f
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2023-12-04 14:15:52 +0530

    chnged

--- node_modules/pdfjs-dist/types/src/display/editor/toolbar.d.ts
+++
@@ -1,9 +0,0 @@
-export class EditorToolbar {
-    static "__#24@#pointerDown"(e: any): void;
-    constructor(editor: any);
-    render(): HTMLDivElement;
-    hide(): void;
-    show(): void;
-    remove(): void;
-    #private;
-}
